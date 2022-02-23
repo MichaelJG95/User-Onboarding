74448ed8843f8ab53e5c7750dcd72623aba92771
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-02-23 15:39:55 -0600

    initial commit

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Form = (props) => {
+    return (
+        <div>
+            <form>
+                <label>Test
+                    <input 
+                        type='text'
+                    />
+                </label>
+            </form>
+        </div>
+    )
+}
+
+export default Form
