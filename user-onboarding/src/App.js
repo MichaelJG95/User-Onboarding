8a2c31663cd25da00d301002b69ba36a86de81b5
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-02-23 16:01:57 -0600

    initial form init commit

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from './components/Form'
 
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  terms: false
+}
+
 function App() {
+  const [formValues, setFormValues] = useState(initialFormValues)
+
   return (
     <div className="App">
       <h1>User</h1>
-      <Form />
+      <Form 
+        values={formValues}
+        />
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,14 +1,51 @@
 import React from 'react'
 
-const Form = (props) => {
+const changeHandler = event => {
+    
+}
+
+const Form = ({values}) => {
     return (
         <div>
             <form>
-                <label>Test
-                    <input 
+                <label>Name: 
+                    <input
+                        name='name' 
                         type='text'
+                        value={values.name}
+                        onChange={changeHandler}
                     />
                 </label>
+
+                <label>Email: 
+                    <input
+                        name='email'
+                        type='email'
+                        value={values.email}
+                        onChange={changeHandler}
+                    />
+                </label>
+
+                <label>Password: 
+                    <input 
+                        name='password'
+                        type='password'
+                        value={values.password}
+                        onChange={changeHandler}
+                    />
+                </label>
+
+                <label>Terms of Service
+                    <input 
+                        name='terms'
+                        type='checkbox'
+                        checked={values.terms}
+                        onChange={changeHandler}
+                    />
+                </label>
+
+                <button>Submit</button>
+
             </form>
         </div>
     )
